259eee8169c2d5a38c1f3f64c5baa999406e37e4
Laila Melo Vaz Lopes <lailamvl@gmail.com>
2024-07-06 18:05:30 -0300

    Commit 4 - Separando Apresentação dos Cálculos

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const { readFileSync } = require('fs');
 
 function gerarFaturaStr (fatura, pecas) {
-    let totalFatura = 0;
-    let creditos = 0;
-    let faturaStr = `Fatura ${fatura.cliente}\n`;
 
     function formatarMoeda(valor) {
       return new Intl.NumberFormat("pt-BR",
@@ -20,7 +17,6 @@ function gerarFaturaStr (fatura, pecas) {
       return creditos;
     }
     
-
     function getPeca(apresentacao){
       return pecas[apresentacao.id];
     }
@@ -48,21 +44,32 @@ function gerarFaturaStr (fatura, pecas) {
       return total;
     }
 
-    for (let apre of fatura.apresentacoes) {
-      // const peca = getPeca(apre);
-      let total = calcularTotalApresentacao(apre);
-  
-      // créditos para próximas contratações
-      creditos += calcularCredito(apre);
-  
-      // mais uma linha da fatura
-      faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(total)} (${apre.audiencia} assentos)\n`;
-      totalFatura += total;
+    function calcularTotalFatura(){
+      let totalFatura = 0;
+      for(let apre of fatura.apresentacoes){
+        totalFatura += calcularTotalApresentacao(apre);
+      }
+      return totalFatura
     }
-    faturaStr += `Valor total: ${formatarMoeda(totalFatura)}\n`;
-    faturaStr += `Créditos acumulados: ${creditos} \n`;
+
+    function calcularTotalCredito(){
+      let creditos = 0;
+      for(let apre of fatura.apresentacoes){
+        creditos += calcularCredito(apre);
+      }
+      return creditos;
+    }
+
+    let faturaStr = `Fatura ${fatura.cliente}\n`;
+
+    for (let apre of fatura.apresentacoes) {
+      faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)\n`;
+    }
+    
+    faturaStr += `Valor total: ${formatarMoeda(calcularTotalFatura())}\n`;
+    faturaStr += `Créditos acumulados: ${calcularTotalCredito()} \n`;
     return faturaStr;
-  }
+}
 
 const faturas = JSON.parse(readFileSync('./faturas.json'));
 const pecas = JSON.parse(readFileSync('./pecas.json'));
